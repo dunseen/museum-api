27c6a4f8612e6219f907fb60fbf96dc14edacee3
Davys Lima <davysjunior08@hotmail.com>
2025-01-10 00:34:51 -0300

    feat: implement characteristic DTO and factory for improved data handling

--- src/characteristics/infrastructure/persistence/relational/mappers/characteristic.mapper.ts
+++ src/characteristics/infrastructure/persistence/relational/mappers/characteristic.mapper.ts
@@ -1,36 +1,54 @@
+import { CharacteristicType } from '../../../../../characteristic-types/domain/characteristic-type';
 import { CharacteristicTypeEntity } from '../../../../../characteristic-types/infrastructure/persistence/relational/entities/characteristic-type.entity';
 import { Characteristic } from '../../../../domain/characteristic';
 import { CharacteristicEntity } from '../entities/characteristic.entity';
 
 export class CharacteristicMapper {
   static toDomain(raw: CharacteristicEntity): Characteristic {
-    const domainEntity = new Characteristic();
-    domainEntity.id = raw.id;
-    domainEntity.name = raw.name;
-    domainEntity.description = raw.description;
-    domainEntity.type = raw.type;
-    domainEntity.createdAt = raw.createdAt;
-    domainEntity.updatedAt = raw.updatedAt;
+    const type = CharacteristicType.create(
+      raw.type.name,
+      raw.type.id,
+      raw.type.createdAt,
+      raw.type.updatedAt,
+    );
+
+    const domainEntity = Characteristic.create(
+      raw.name,
+      raw.description,
+      type,
+      raw.files,
+      raw.createdAt,
+      raw.updatedAt,
+      raw.id,
+    );
 
     return domainEntity;
   }
 
-  static toPersistence(domainEntity: Characteristic): CharacteristicEntity {
+  static toPersistence(
+    domainEntity: Partial<Characteristic>,
+  ): CharacteristicEntity {
     const persistenceEntity = new CharacteristicEntity();
     const type = new CharacteristicTypeEntity();
 
     if (domainEntity.id) {
       persistenceEntity.id = Number(domainEntity.id);
     }
-    if (domainEntity.type.id) {
+    if (domainEntity?.type?.id) {
       type.id = Number(domainEntity.type.id);
     }
 
-    persistenceEntity.name = domainEntity.name;
-    persistenceEntity.description = domainEntity.description;
+    if (domainEntity?.type?.name) {
+      type.name = domainEntity.type.name;
+    }
+
+    if (domainEntity?.description) {
+      persistenceEntity.description = domainEntity.description;
+    }
+
     persistenceEntity.type = type;
-    persistenceEntity.createdAt = domainEntity.createdAt;
-    persistenceEntity.updatedAt = domainEntity.updatedAt;
+    persistenceEntity.createdAt = domainEntity.createdAt ?? new Date();
+    persistenceEntity.updatedAt = domainEntity.updatedAt ?? new Date();
 
     return persistenceEntity;
   }
