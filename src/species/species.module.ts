6deea713c951102954204c3c9973104e0139e248
Davys Lima <davysjunior08@hotmail.com>
2025-03-25 00:59:02 -0300

    feat: update module imports to use forwardRef for circular dependency resolution

--- src/species/species.module.ts
+++ src/species/species.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SpeciesService } from './species.service';
 import { RelationalSpeciePersistenceModule } from './infrastructure/persistence/relational/relational-persistence.module';
 import { RelationalCharacteristicPersistenceModule } from '../characteristics/infrastructure/persistence/relational/relational-persistence.module';
 import { RelationalTaxonPersistenceModule } from '../taxons/infrastructure/persistence/relational/relational-persistence.module';
 import { RelationalFilePersistenceModule } from '../files/infrastructure/persistence/relational/relational-persistence.module';
+import { FileMinioModule } from '../files/infrastructure/uploader/minio/files.module';
+import { PostsModule } from '../posts/posts.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { RelationalFilePersistenceModule } from '../files/infrastructure/persist
     RelationalCharacteristicPersistenceModule,
     RelationalTaxonPersistenceModule,
     RelationalFilePersistenceModule,
+    FileMinioModule,
+    forwardRef(() => PostsModule),
   ],
   controllers: [],
   providers: [SpeciesService],
