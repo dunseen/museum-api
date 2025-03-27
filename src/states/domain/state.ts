eb22b557ab44213c8531bb5f2e023e3891d75748
Davys Lima <davysjunior08@hotmail.com>
2025-03-27 19:18:13 -0300

    feat: add example values for state properties in the State model

--- src/states/domain/state.ts
+++ src/states/domain/state.ts
@@ -3,11 +3,18 @@ import { ApiProperty } from '@nestjs/swagger';
 export class State {
   @ApiProperty({
     type: Number,
+    example: 1,
   })
   id: number;
 
   @ApiProperty({
     type: String,
+    example: 'Pará',
   })
   name: string;
+  @ApiProperty({
+    type: String,
+    example: 'PA',
+  })
+  code: string;
 }
