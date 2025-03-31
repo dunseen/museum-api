aa29974264a802fe455be0dae01da024b0da2466
Davys Lima <davysjunior08@hotmail.com>
2025-03-31 10:42:43 -0300

    fix(posts): fix posts list dashboard

--- src/posts/infrastructure/persistence/relational/repositories/post.repository.ts
+++ src/posts/infrastructure/persistence/relational/repositories/post.repository.ts
@@ -123,17 +123,59 @@ export class PostRelationalRepository implements PostRepository {
   }: {
     paginationOptions: IPaginationOptions;
   }): Promise<WithCountList<Post>> {
-    const [entities, totalCount] = await this.postRepository.findAndCount({
-      skip: (paginationOptions.page - 1) * paginationOptions.limit,
-      take: paginationOptions.limit,
-      relations: ['author', 'validator', 'specie'],
-      loadEagerRelations: true,
-      order: {
-        updatedAt: 'DESC',
-      },
-    });
+    const query = this.postRepository
+      .createQueryBuilder('p')
+      .innerJoinAndSelect('p.specie', 's')
+      .leftJoinAndSelect('p.author', 'author')
+      .leftJoinAndSelect('p.validator', 'validator')
+      .leftJoinAndSelect('s.characteristics', 'c')
+      .leftJoinAndSelect('c.type', 'type')
+      .leftJoinAndSelect('s.files', 'f')
+      .innerJoinAndSelect('s.taxons', 't')
+      .leftJoinAndSelect('s.city', 'city')
+      .leftJoinAndSelect('s.state', 'state')
+      .innerJoinAndSelect('t.hierarchy', 'h')
+      .skip((paginationOptions.page - 1) * paginationOptions.limit)
+      .take(paginationOptions.limit);
 
-    return [entities.map((user) => PostMapper.toDomain(user)), totalCount];
+    if (paginationOptions.filters?.name) {
+      query.andWhere(
+        'LOWER(s.scientificName) LIKE LOWER(:name) OR LOWER(s.commonName) LIKE LOWER(:name)',
+        {
+          name: `%${paginationOptions.filters.name}%`,
+        },
+      );
+    }
+
+    if (paginationOptions.filters?.family) {
+      query.andWhere(
+        'LOWER(h.name) = :hierarchyFamily AND LOWER(t.name) LIKE LOWER(:family)',
+        {
+          hierarchyFamily: 'family',
+          family: `%${paginationOptions.filters.family}%`,
+        },
+      );
+    }
+
+    if (paginationOptions.filters?.genus) {
+      query.andWhere(
+        'LOWER(h.name) = :hierarchyGenus AND LOWER(t.name) LIKE LOWER(:genus)',
+        {
+          hierarchyGenus: 'genus',
+          genus: `%${paginationOptions.filters.genus}%`,
+        },
+      );
+    }
+
+    if (paginationOptions.filters?.characteristicIds?.length) {
+      query.andWhere('c.id IN (:...ids)', {
+        ids: paginationOptions.filters.characteristicIds,
+      });
+    }
+
+    const [entities, totalCount] = await query.getManyAndCount();
+
+    return [entities.map(PostMapper.toDomain), totalCount];
   }
 
   async findById(id: Post['id']): Promise<NullableType<Post>> {
