5aee69f2a13c9035e1fa0a786c315d1725a83bbd
Davys Lima <davysjunior08@hotmail.com>
2025-05-17 11:23:05 -0300

    feat(security): add helmet middleware for enhanced security

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import { AppModule } from './app.module';
 import validationOptions from './utils/validation-options';
 import { AllConfigType } from './config/config.type';
 import { ResolvePromisesInterceptor } from './utils/serializer.interceptor';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -35,6 +36,7 @@ async function bootstrap() {
     new ResolvePromisesInterceptor(),
     new ClassSerializerInterceptor(app.get(Reflector)),
   );
+  app.use(helmet());
 
   const options = new DocumentBuilder()
     .setTitle('API')
