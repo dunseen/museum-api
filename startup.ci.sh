cca9fa30c53ef9bfdfdcf353ce96e5fd00a44083
Davys Lima <davysjunior08@hotmail.com>
2025-11-01 12:09:25 -0300

    chore: comment out seed run

--- startup.ci.sh
+++ startup.ci.sh
@@ -3,9 +3,9 @@ set -e
 
 /opt/wait-for-it.sh postgres:5432
 npm run migration:run
-npm run seed:run
+# npm run seed:run
 npm run start:prod > prod.log 2>&1 &
-/opt/wait-for-it.sh maildev:1080
+# /opt/wait-for-it.sh maildev:1080
 /opt/wait-for-it.sh localhost:3000
 npm run lint
 npm run test:e2e -- --runInBand
