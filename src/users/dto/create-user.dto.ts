ce5d0584599f53f299bdf935db98fe787a2b0655
Davys Lima <davysjunior08@hotmail.com>
2025-11-06 23:02:51 -0300

    refactor: clean up CreateUserDto by removing unused password field and unnecessary imports

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,6 @@
 import { Transform, Type } from 'class-transformer';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import {
-  // decorators here
-  IsEmail,
-  IsNotEmpty,
-  IsOptional,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
 import { RoleDto } from '../../roles/dto/role.dto';
 import { StatusDto } from '../../statuses/dto/status.dto';
 import { lowerCaseTransformer } from '../../utils/transformers/lower-case.transformer';
@@ -17,10 +11,6 @@ export class CreateUserDto {
   @IsNotEmpty()
   @IsEmail()
   email: string;
-
-  @ApiProperty()
-  @MinLength(6)
-  password?: string;
 
   @ApiProperty({ example: 'John', type: String })
   @IsNotEmpty()
